eaeaded88da9a26d033a8f6c8e140f73c05ee64f
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-06 23:03:31 +0545

    Hide NavBar/header in Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 import { Router, Stack, Scene } from 'react-native-router-flux';
 
 import Home from './screens/Home';
@@ -9,22 +8,11 @@ function App() {
   return (
     <Router>
       <Stack key='root'>
-        <Scene key='home' component={Home} title='Home' />
+        <Scene key='home' component={Home} title='Home' hideNavBar={true} />
         <Scene key='detail' component={Detail} title='Detail' />
       </Stack>
     </Router>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textStyle: {
-    fontSize: 20
-  }
-});
 
 export default App;
