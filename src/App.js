47a3aa64c4e707f1338dbe5612b0c6e0a9278333
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-06 23:08:05 +0545

    Take some space from Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { Router, Stack, Scene } from 'react-native-router-flux';
 
 import Home from './screens/Home';
@@ -6,13 +7,36 @@ import Detail from './screens/Detail';
 
 function App() {
   return (
-    <Router>
-      <Stack key='root'>
-        <Scene key='home' component={Home} title='Home' hideNavBar={true} />
-        <Scene key='detail' component={Detail} title='Detail' />
-      </Stack>
-    </Router>
+    <View style={styles.container}>
+      <View style={styles.takeSpace}>
+        <Text style={styles.text}>Just taking up space</Text>
+      </View>
+      <Router>
+        <Stack key='root'>
+          <Scene key='home' component={Home} title='Home' hideNavBar={true} />
+          <Scene key='detail' component={Detail} title='Detail' />
+        </Stack>
+      </Router>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  takeSpace: {
+    flex: 0.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blueviolet'
+  },
+  text: {
+    fontSize: 20,
+    color: 'white'
+  }
+});
 
 export default App;
