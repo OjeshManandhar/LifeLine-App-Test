635cf60b2f3a8106d7a63886796d2bd8bf0ed495
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-13 22:23:03 +0545

    Call Mapbox Geocoder API

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -5,8 +5,37 @@ import { View, StyleSheet } from 'react-native';
 import Map from './../component/Map';
 import SearchBox from './../component/SearchBox';
 
+import { MAPBOX_API_KEY } from 'react-native-dotenv';
+
 function searchLocation(keyword) {
   console.log('keyword:', keyword);
+  geocoder(keyword);
+}
+
+function geocoder(keyword) {
+  const wordsList = keyword.split(' ').join('+');
+
+  console.log(wordsList);
+
+  // GET request
+  fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${wordsList}.json?country=np&access_token=${MAPBOX_API_KEY}`,
+    {
+      method: 'GET'
+      //Request Type
+    }
+  )
+    .then(response => response.json())
+    //If response is in json then in success
+    .then(responseJson => {
+      //Success
+      console.log('response:', responseJson);
+    })
+    //If response is not in json then in error
+    .catch(error => {
+      //Error
+      console.error('error:', error);
+    });
 }
 
 function MapScreen(props) {
