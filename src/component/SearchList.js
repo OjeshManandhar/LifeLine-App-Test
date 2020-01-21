0d6e6751b876b6215c6071bef29060b1d9b9315f
OjeshManandhar <ozes.manandhar@gmail.com>
2020-01-21 12:51:55 +0545

    Create SearchList

---
+++ src/component/SearchList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function SearchList() {
+  return (
+    <View style={styles.container}>
+      <Text>SearchList</Text>
+      <Text>SearchList</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch'
+  }
+});
