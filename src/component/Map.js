cac542083a325dab78963564e6dcb873558c1622
OjeshManandhar <ozes.manandhar@gmail.com>
2020-03-07 07:08:19 +0545

    Show coordinates of clicked location

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 // packages
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
-function Map({ userInfo, destination }) {
-  console.log('userInfo:', JSON.parse(userInfo));
-  console.log('destination:', destination);
+function Map({ userInfo, destination, ...props }) {
+  const mapRef = React.createRef();
+
+  const [centerCoordinate, setCenterCoordinate] = useState(null);
+
+  function handlePress(event) {
+    const pointInView = event.geometry.coordinates;
+
+    console.log('clicked point:', pointInView);
+
+    setCenterCoordinate(pointInView);
+    props.setPickedLocation(pointInView);
+  }
 
   function renderPointAnnotation() {
     return (
@@ -27,9 +37,11 @@ function Map({ userInfo, destination }) {
   return (
     <View style={styles.container}>
       <MapboxGL.MapView
+        ref={mapRef}
         // A size must be provided to the MapboxGL.MapView through style prop
         style={styles.container}
         styleURL={MapboxGL.StyleURL.Outdoors}
+        onPress={event => props.mapStatus === 'picking' && handlePress(event)}
       >
         <MapboxGL.UserLocation visible />
 
