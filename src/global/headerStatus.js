049604a568d1c94910e247b10a7491fe946f4ef1
OjeshManandhar <ozes.manandhar@gmail.com>
2020-05-13 17:26:58 +0545

    Adjust Searchbox container styles

---
+++ src/global/headerStatus.js
@@ -0,0 +1,7 @@
+const MapScreenHeaderStatus = Object.freeze({
+  mapView: 0,
+  searching: 1,
+  picking: 2
+});
+
+export default MapScreenHeaderStatus;
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 // components
 import Map from 'component/Map';
+import SearchBox from 'component/SearchBox';
 
-// utils
-import zIndex from 'utils/zIndex';
+// global
+import ZIndex from 'global/zIndex';
+import MapScreenHeaderStatus from 'global/headerStatus';
 
 function MapScreen(props) {
+  const [headerStatus, setHeaderStatus] = useState(
+    MapScreenHeaderStatus.mapView
+  );
+
   return (
     <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <Text>SearchBox</Text>
-      </View>
+      {(headerStatus === MapScreenHeaderStatus.mapView ||
+        headerStatus === MapScreenHeaderStatus.seaerching) && (
+        <View style={styles.searchContainer}>
+          <SearchBox />
+        </View>
+      )}
 
       <Map />
     </View>
@@ -25,12 +34,32 @@ const styles = StyleSheet.create({
   },
   searchContainer: {
     position: 'absolute',
-    top: 10,
-    left: '5%',
-    right: '5%',
+    top: '1%',
+    left: '2.5%',
+    right: '2.5%',
     height: 50,
-    borderWidth: 2,
-    zIndex: zIndex.searchBox
+    zIndex: ZIndex.searchBox,
+
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+
+    borderRadius: 4,
+    borderWidth: 0.25,
+    borderColor: '#555555',
+    backgroundColor: '#ffffff',
+
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: -1,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 1,
+    elevation: 5
   }
 });
 
