39d386cc7ac089e2b03f31094b213caa55a3d825
OjeshManandhar <ozes.manandhar@gmail.com>
2020-05-13 16:46:45 +0545

    Ask for FINE ACCESS Permission
    Won't check if GPS is on or not even if access is granted

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,11 +1,42 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, PermissionsAndroid } from 'react-native';
 
 // packages
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
 function Map() {
-  useEffect(() => {}, []);
+  async function askGPSPermissions() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'App GPS Permission',
+          message:
+            'App needs access to your location (GPS & Internet) ' +
+            'so we can pin-point your exact location.',
+          buttonNegative: 'No, thanks',
+          buttonPositive: 'OK'
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('FINE LOCATION Access Granted');
+      } else {
+        console.log('FINE LOCATION Access Denied');
+      }
+    } catch (err) {
+      console.warn('FINE ACCESS Permission error:', err);
+    }
+  }
+
+  useEffect(() => {
+    PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+    ).then(result => {
+      if (!result) {
+        askGPSPermissions();
+      }
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
